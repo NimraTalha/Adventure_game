25509b7050833b11640ddc9f3423e7db535a7498
NimraTalha <nimratalha@gmail.com>
2024-05-07 23:21:23 +0500

    your message

---
+++ main.js
@@ -0,0 +1,83 @@
+import inquirer from "inquirer";
+class Player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    attack(opponent) {
+        const damage = Math.floor(Math.random() * 20) + 1;
+        console.log(`You attack ${opponent.name} and deal ${damage} damage!`);
+        opponent.fuel -= damage;
+    }
+    drinkPotion() {
+        const potionAmount = Math.floor(Math.random() * 20) + 1;
+        console.log(`You drink a potion and restore ${potionAmount} fuel.`);
+        this.fuel += potionAmount;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    attack(player) {
+        const damage = Math.floor(Math.random() * 20) + 1;
+        console.log(`${this.name} attacks you and deals ${damage} damage!`);
+        player.fuel -= damage;
+    }
+}
+async function main() {
+    console.log("Welcome to the Battle Game!");
+    // Get player's name
+    const { name } = await inquirer.prompt([{
+            name: "name",
+            type: "input",
+            message: "Please enter your name.."
+        }]);
+    const player = new Player(name);
+    // Select opponent
+    const { opponentName } = await inquirer.prompt([{
+            name: "opponentName",
+            type: "list",
+            message: "Select your opponent..",
+            choices: ["skeleton", "snake", "Zombie"]
+        }]);
+    const opponent = new Opponent(opponentName);
+    console.log(`You (${player.name}) vs ${opponent.name}`);
+    // Game loop
+    while (player.fuel > 0 && opponent.fuel > 0) {
+        // Prompt player for action
+        const { action } = await inquirer.prompt([{
+                name: "action",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["Attack", "Drink potion", "Run away"]
+            }]);
+        // Perform player's chosen action
+        switch (action) {
+            case "Attack":
+                player.attack(opponent);
+                break;
+            case "Drink potion":
+                player.drinkPotion();
+                break;
+            case "Run away":
+                console.log("You run away!");
+                return; // End the game
+        }
+        // If opponent still has fuel, let them attack
+        if (opponent.fuel > 0) {
+            opponent.attack(player);
+        }
+    }
+    // Game over
+    if (player.fuel <= 0) {
+        console.log("You lose!");
+    }
+    else {
+        console.log("You win!");
+    }
+}
+main();
